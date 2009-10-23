6791f364264ad2a526c2a961b4b690f5fad18b00
Jacob Persson <jacob@meck.(none)>
2009-10-24 00:27:32 +0200

    Created the skeleton for the meter view.

---
+++ PowerUsageApp/Classes/CCDCMeterViewController.h
@@ -0,0 +1,16 @@
+//
+//  CCDCMeterViewController.h
+//  PowerUsageApp
+//
+//  Created by Jacob Persson on 10/24/09.
+//  Copyright 2009 RobotCrowd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface CCDCMeterViewController : UIViewController {
+  
+}
+
+@end
